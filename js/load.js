90b20cea7c4af760621293c5ed469c4cd1a0c886
Rahul Subhash Shinde <rshinde@mica.edu>
2016-03-20 14:02:52 -0400

    final push

--- js/load.js
+++ js/load.js
@@ -4,6 +4,25 @@ $(document).ready( function() {
 
     $("#question").on("click", function() {
         $("#about-desc").fadeToggle('slow');
+        $("#question").fadeToggle('fast');
+        $("#question-close").fadeToggle('slow');
+    });
+
+    $("#question-close").on("click", function() {
+        $("#about-desc").fadeToggle('slow');
+        $("#question").fadeToggle('slow');
+        $("#question-close").fadeToggle('fast');
+    });
+
+    $(window).scroll(function(){
+      if($(window).scrollTop() > 100){
+          $("#top").fadeToggle('slow');
+    });
+
+    $(window).scroll(function(){
+      if($(window).scrollTop() < 100){
+          $("#top").fadeToggle('slow');
+      }
     });
 
 });
